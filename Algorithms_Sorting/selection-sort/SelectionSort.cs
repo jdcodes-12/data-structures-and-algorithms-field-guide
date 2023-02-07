6b7dab581e5625fca646c039d5df5a795ffcea1d
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-02-07 10:33:15 -0600

    Add C# selection sort implementation

---
+++ Algorithms_Sorting/selection-sort/SelectionSort.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Algorithms.Sorting
+{
+    class SelectionSort { 
+        static void Main(String[] args)
+        {
+            int[] numbers = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Console.Write("Unsorted List: ");
+            DisplayList(numbers);
+            RunSelectionSort(numbers);
+            Console.Write("Sorted List: ");
+            DisplayList(numbers);
+        }
+
+        static void DisplayList(int[] numbers)
+        {
+            
+            int moves = 0;
+            Console.Write("[");
+            foreach (int number in numbers)
+            {
+                if (moves == numbers.Length - 1)
+                    Console.Write(number);
+                
+                else
+                {
+                    Console.Write($"{number}, ");
+                    moves++;
+                }
+            }
+            Console.WriteLine("]");
+        }
+
+        static void RunSelectionSort(int[] unsortedArr)
+        {
+            int minIndex, i, j, temp;
+            int n = unsortedArr.Length;
+
+            for (i = 0; i < n; i++)
+            {
+                minIndex = i;
+                for (j = i+1; j < n; j++)
+                    if (unsortedArr[j] < unsortedArr[i])
+                        minIndex = j;
+
+                // Swap
+                temp = unsortedArr[i];
+                unsortedArr[i] = unsortedArr[minIndex];
+                unsortedArr[minIndex] = temp;
+            }
+        }
+    }
+}
