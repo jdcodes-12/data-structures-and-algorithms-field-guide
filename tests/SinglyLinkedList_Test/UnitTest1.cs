1801f2c7e584467ace84bffe5da1d2aa1e36d61f
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-02-11 23:46:19 -0600

    Add NUnit Project for SinglyLinkedList

---
+++ tests/SinglyLinkedList_Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SinglyLinkedList_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
