37567b5878bc58bc491bb7ae799192a78e3f7b87
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-02-14 16:28:34 -0600

    Push to fix git log

--- data-structures/02-linked-lists/list-implementations/01-singly-linked/C#/src/SinglyLinkedListDriver.cs
+++ data-structures/02-linked-lists/list-implementations/01-singly-linked/C#/src/SinglyLinkedListDriver.cs
@@ -1,28 +1,30 @@
-﻿#region
-using System.Xml.Linq;
+#region
 using static System.Console;
+using DataStructuresLib;
 #endregion
 
-namespace SinglyLinkedList
-{ 
-    class SinglyLinkedListDriver {
+namespace SinglyLinkedListImp
+{
+    class SinglyLinkedListDriver
+    {
         static void Main()
         {
             SinglyLinkedList list = new();
             DisplayListState(list);
 
-            Node node_a = CreateNodeWithInternalDataOf(5);
-            Node node_b = CreateNodeWithInternalDataOf(10);
-            Node node_c = CreateNodeWithInternalDataOf(15);
-            Node node_d = CreateNodeWithInternalDataOf(20);
-            Node node_e = CreateNodeWithInternalDataOf(25);
-            Node node_f = CreateNodeWithInternalDataOf(30);
-            Node node_g = CreateNodeWithInternalDataOf(35);
+            SingleLinkNode node_a = CreateNodeWithInternalDataOf(5);
+            SingleLinkNode node_b = CreateNodeWithInternalDataOf(10);
+            SingleLinkNode node_c = CreateNodeWithInternalDataOf(15);
+            SingleLinkNode node_d = CreateNodeWithInternalDataOf(20);
+            SingleLinkNode node_e = CreateNodeWithInternalDataOf(25);
+            SingleLinkNode node_f = CreateNodeWithInternalDataOf(30);
+            SingleLinkNode node_g = CreateNodeWithInternalDataOf(35);
 
             WriteLine("Populating list with nodes...");
-            Node[] nodes = { node_a, node_b, node_c, node_d, node_e };
-           
-            foreach (Node node in nodes) {
+            SingleLinkNode[] nodes = { node_a, node_b, node_c, node_d, node_e };
+
+            foreach (SingleLinkNode node in nodes)
+            {
                 AppendNode(node, list);
             }
             WriteLine("Done populating.\n");
@@ -49,33 +51,33 @@ namespace SinglyLinkedList
             ReadKey();
         }
 
-        private static Node CreateNodeWithInternalDataOf(int data)
+        private static SingleLinkNode CreateNodeWithInternalDataOf(int data)
         {
-            Node node = new(data);
+            SingleLinkNode node = new(data);
             WriteLine("\nCreating new node...");
             WriteLine($"New Node Contents: {node.StringifyNode()}");
             return node;
         }
-        
+
         private static void DisplayListState(SinglyLinkedList list)
         {
             WriteLine($"\nCurrent list size: {list.Size}");
             WriteLine($"Current Head node: {(list.HeadIsNull() ? "null" : list.Head!.StringifyNode())}");
         }
 
-        private static void PrependNode(Node node, SinglyLinkedList list)
+        private static void PrependNode(SingleLinkNode node, SinglyLinkedList list)
         {
             WriteLine($"\nPrepending {node.StringifyNode()} to list...");
             WriteLine($"Node was prepended: {list.PrependNode(node)}");
         }
 
-        private static void AppendNode(Node node, SinglyLinkedList list)
+        private static void AppendNode(SingleLinkNode node, SinglyLinkedList list)
         {
             WriteLine($"\nAppending ${node.StringifyNode()} to list...");
             WriteLine($"Node was appended: {list.AppendNode(node)}");
         }
 
-        private static void InsertNodeAtPosition(Node node, int position, SinglyLinkedList list)
+        private static void InsertNodeAtPosition(SingleLinkNode node, int position, SinglyLinkedList list)
         {
             WriteLine($"\nInserting {node.StringifyNode()} to list at position ${position}...");
             WriteLine($"Node was inserted: {list.InsertNodeAtPosition(node, position)}");
