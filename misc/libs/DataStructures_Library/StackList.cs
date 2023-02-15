abd5359141c144720835675221f2b72f3cf38715
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-02-15 13:56:14 -0600

    Implement StackList methods
    Added logic and unit tests for Push() and Peek() in StackList

--- misc/libs/DataStructures_Library/StackList.cs
+++ misc/libs/DataStructures_Library/StackList.cs
@@ -2,6 +2,76 @@ namespace DataStructuresLib
 {
     public class StackList
     {
+        public int Size { get; set; }
+        public DoubleLinkNode? Top { get; set; }
+        public DoubleLinkNode? Base { get; set; }
 
+        public StackList()
+        {
+            Size = 0;
+            Top = null;
+        }
+        
+        public bool Push(DoubleLinkNode? node)
+        {
+            if (NodeIsNull(node)) return false;
+
+            else if (IsEmpty())
+            {
+                Base = node;
+                Top = node;
+                Size++;
+                return true;
+            }
+
+            else if (OnlyOneNodeInStack())
+            {
+                Base.NextLink = node;
+                node.PrevLink = Base;
+                Top = node;
+                Size++;
+                return true;
+            }
+
+            else
+            {
+                Top.NextLink = node;
+                node.PrevLink = Top;
+                Top = node;
+                Size++;
+                return true;
+            }
+        }
+        
+       
+        public DoubleLinkNode Peek()
+        {
+            if (IsEmpty()) return null;
+            else return Top;
+        }
+        // PeekAtPosition()
+        // Push()
+        // Pop()
+        // ClearStack()
+        // DisplayStack()
+        
+        public bool IsEmpty() => Size <= 0;
+
+        // --- Utility Functions ---
+
+        // Node Checks
+        private bool NodeIsNull(DoubleLinkNode node) => node == null;
+        private bool OnlyOneNodeInStack() => Size == 1;
+
+        // Traversals
+        //private DoubleLinkNode TraverseStack_TopDown()
+        //{
+        //   // start from top
+        //   // while top.prevLink != null
+        //    // move top = top.prevLink
+
+        //  // return bottomNode
+        //}
+        
     }
 }
